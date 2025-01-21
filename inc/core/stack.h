86a2a623a965dff0839dbcd5403542460b49391e
CPoet <llzero54@foxmail.com>
2025-01-21 17:03:32 +0800

    fix: 补充注释

--- inc/core/stack.h
+++ inc/core/stack.h
@@ -17,18 +17,38 @@ typedef struct {
     POINTER*values;
 } Stack;
 
+/// 实例化栈
+/// @param valueSize 元素大小
 Stack *stackNew(int valueSize);
 
+/// 实例化栈
+/// @param initSize 初始化大小
+/// @param valueSize 元素大小
 Stack *stackNewByInit(int initSize, int valueSize);
 
+/// 元素出栈
+/// @param stack 栈
+/// @return 元素
 POINTER stackPop(Stack *stack);
 
+/// 查看栈顶元素
+/// @param stack 栈
+/// @return 元素
 POINTER stackPeek(Stack *stack);
 
+/// 添加元素
+/// @param stack 栈
+/// @param val 元素
+/// @return 是否添加成功
 BOOL stackPush(Stack *stack, POINTER val);
 
+/// 判断栈是否为空
+/// @param stack 栈
+/// @return 是否为空
 BOOL stackEmpty(Stack *stack);
 
+/// 销毁栈
+/// @param stack 栈
 void stackDel(Stack *stack);
 
 #endif
